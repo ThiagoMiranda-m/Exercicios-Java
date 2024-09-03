7b139de40655fbf7b19c57562d99d75631eb34a9
ThiagoMiranda-m <thiagoanm75@gmail.com>
2024-09-03 17:09:07 -0300

    mensagem

---
+++ Exercicio 5/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       Scanner scan = new Scanner(System.in);
+       
+       int A = scan.nextInt();
+       int B = scan.nextInt();
+       int C = A * B;
+
+       System.out.println("PROD = " + C);
+    }
+}
