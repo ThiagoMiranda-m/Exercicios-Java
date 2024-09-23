58cde2d063537a28ec2a28ffb5efbb998e50e73e
Thiago Andre <thiagoanm75@gmail.com>
2024-09-23 16:47:17 -0300

    Main.java

--- Exercicio 1/src/Main.java
+++ Exercicio 1/src/Main.java
@@ -6,8 +6,6 @@ public class Main {
  
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        
-        System.out.println("Insira o valor do raio: ");
         double raio = scan.nextDouble();
         double area = 3.14159 * (raio * raio);
         
