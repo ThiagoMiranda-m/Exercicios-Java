cb19d5dbedc72051ad1609e1049210ce69b0daea
Thiago Andre <thiagoanm75@gmail.com>
2024-09-23 16:46:21 -0300

    Main.java

--- Exercicio 1/src/Main.java
+++ Exercicio 1/src/Main.java
@@ -1,9 +1,10 @@
 //A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
-
+import java.io.IOException;
 import java.util.Scanner;
-
-class Main {
-    public static void main(String[] args) {
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         
         System.out.println("Insira o valor do raio: ");
@@ -12,5 +13,7 @@ class Main {
         
         System.out.printf("A=%.4f\n", area);
 
+ 
     }
+ 
 }
